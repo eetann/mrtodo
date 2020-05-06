5980bfd4b29b1e5b8d5f36dff8a2f96cadaff963
eetann <hideharu092@gmail.com>
2020-05-06 22:39:18 +0900

    class が使えるようなのでclassで書き換えた #1

--- task.js
+++ task.js
@@ -1,29 +1,31 @@
 /**
+ * yyyy-MM-dd HH mm の3つの文字列からDateデータを生成
+ */
+function getDateBy3Str(yyyyMMdd, HH, mm) {
+  return new Date(yyyyMMdd.replace(/-/g, '/') + ' ' + HH + ':' + mm + ':00');
+}
+
+/**
  * タスククラス
  */
-function Task(values) {
-  /* プロパティの整理 */
-  this.name = values.name.name.value;
-  // remind を設定
-  // NOTE: 変数のスコープに注意
-  this.remind;
-  Task.prototype.setRemind = function () {
+class Task {
+  constructor(values) {
+    this.name = values.name.name.value;
+    // remind を設定
     if (values.days) {
-      this.remind = new Date(values.days.day.selected_date.replace(/-/g, '/') + ' '
-        + values.hours.hour.selected_option.value + ':'
-        + values.minutes.minute.selected_option.value + ':00');
+      this.remind = getDateBy3Str(values.days.day.selected_date,
+        values.hours.hour.selected_option.value,
+        values.minutes.minute.selected_option.value);
     } else {
       // this.remind = new Date();
       // this.remind.setSeconds(remind.getSeconds() + 15);
       this.remind = '';
     }
+    this.start = '';
+    this.end = '';
   }
-  this.setRemind();
-  this.start = '';
-  this.end = '';
-
   /* タスク内容をslackへ投稿 */
-  Task.prototype.postTask = function () {
+  postTask() {
     var msg = {
       "blocks": [
         {
